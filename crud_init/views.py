427c64ca04b0bd05a52372c5e792c1e8091c0c94
Gabrielsuncin <gc.everit@itmss.com.br>
2020-11-05 18:30:57 -0300

    updating create

--- crud_init/views.py
+++ crud_init/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.files.storage import FileSystemStorage
 from .models import Agenda, Person
 from .forms import PersonForm
 
 
 # Create your views here.
 def home(request):
-    form = PersonForm()
     context = {
         'page_title': 'Agenda',
         'header_title': 'Organize sua agenda adicionando, editando e excluindo contatos existentes!',
         'persons': Person.objects.all(),
+    }
+    return render(request, 'homepage.html', context)
+
+
+def add_contact(request):
+    form = PersonForm()
+    context = {
+        'page_title': 'Novo contato',
+        'header_title': 'Aumente sua rede e conheça novas pessoas',
         'form': form,
     }
+    # if request.POST and request.FILES['photo']:
+    #     form = PersonForm(request.POST and request.FILES)
+    #     photo = request.FILES['photo']
+    #     fs = FileSystemStorage()
+    #     fs.save(photo.name, photo)
+    #     form.save()
+    #     context['message'] = 'Adicionado com sucesso'
+    #     return redirect(reverse('home'))
     if request.POST:
         form = PersonForm(request.POST)
         form.save()
         context['message'] = 'Adicionado com sucesso'
         return redirect(reverse('home'))
-    return render(request, 'homepage.html', context)
+    return render(request, 'add_contact.html', context)
